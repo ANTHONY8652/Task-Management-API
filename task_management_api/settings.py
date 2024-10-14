c626e6ff900f91110f833dbf160fa372b7a52aa2
ANTHONY8652 <githinjianthony720@gmail.com>
2024-10-14 15:59:36 +0300

    Added users app

--- task_management_api/settings.py
+++ task_management_api/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'task_api',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- task_management_api/urls.py
+++ task_management_api/urls.py
@@ -17,8 +17,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Task Management API',
+        default_version = '1.0.0',
+        description = 'A Task management API that provides an endpoint for managing tasks, task status. This API enables seamless integration with front-edn applications.',
+        terms_of_service = 'https://www.google.com/policies/terms',
+        contact = openapi.Contact(email='githinjianthony720@gmail.com'),
+        license = openapi.License(name='MIT License'),
+    ),
+    public = True,
+    permission_classes = (permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task_api.urls')),
+    path('api/', include('task_api.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-with-ui'),
 ]
